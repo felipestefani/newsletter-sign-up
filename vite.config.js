0ad83fb288acf89c8fcd5362fd5eddb78783c37b
Felipe Stefani <felipestefani@MacBook-Pro-de-Felipe.local>
2023-07-27 11:59:28 -0300

    adding dist

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/newsletter-sign-up/',
+  base: '/newsletter-sign-up',
   plugins: [
     vue(),
   ],
