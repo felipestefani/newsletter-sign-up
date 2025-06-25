1efbff667f2bf5a1a93a454e34d50a5470d71582
Felipe Stefani <felipestefani@Felipes-iMac.local>
2025-06-25 12:16:27 -0300

    alterando rota rifa

--- src/router/index.js
+++ src/router/index.js
@@ -3,8 +3,9 @@ import Home from "@/views/Home.vue";
 import SuccessSignUp from "../views/SuccessSignUp.vue";
 
 const routes = [
-    {path: '/', name: 'Home', component: Home},
-    {path: '/success-sign-up', name: 'SuccessSignUp', component: SuccessSignUp},
+    /*{path: '/', name: 'Home', component: Home},
+    {path: '/success-sign-up', name: 'SuccessSignUp', component: SuccessSignUp},*/
+    {path: '/', name: 'SuccessSignUp', component: SuccessSignUp},
 ]
 
 const router = createRouter({
